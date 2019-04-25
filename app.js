2350b6aebf877af0ca0edc42df8a7672474aef45
Pavel Radkevich <pradkevich@elinext.com>
2019-04-25 14:22:42 +0300

    encapsulated dom strings into object

--- app.js
+++ app.js
@@ -5,22 +5,44 @@ var budgetController = (function () {
 
 // UI CONTROLLER
 var UIController = (function () {
-    // for future
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+    };
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
+    var DOMstrings = UICtrl.getDOMstrings();
     var ctrlAddItem = function () {
 
         // Get field input data
+        var input = UICtrl.getInput();
+        console.log(input);
         // Add the item to the budget controller
         // Add the item to the UI
         // Calculate the budget
         // Display the budget on the UI
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOMstrings.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function (event) {
         if (event.keyCode === 13 || event.which === 13) {
