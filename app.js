f03f7dab621df56808c439a35df819e57f5fcc74
Pavel Radkevich <pradkevich@elinext.com>
2019-04-25 13:55:39 +0300

    added eventListeners for input button

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
+// BUDGET CONTROLLER
 var budgetController = (function () {
-
-    var x = 23;
-    var add = function (a) {
-        return x + a;
-    }
-
-    return {
-        publictest: function (b) {
-            console.log(add(b));
-        }
-    }
-
+    //for future
 })();
 
+// UI CONTROLLER
 var UIController = (function () {
-
     // for future
 })();
 
-
+// GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
-    // for future
+    var ctrlAddItem = function () {
+
+        // Get field input data
+        // Add the item to the budget controller
+        // Add the item to the UI
+        // Calculate the budget
+        // Display the budget on the UI
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
+
 })(budgetController, UIController);
